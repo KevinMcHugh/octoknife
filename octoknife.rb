30b2260a0279de3732163b06ace83ed631ed27ae
Kevin McHugh <kev@kevinmchugh.me>
2021-03-30 14:28:06 -0500

    Adds a bad first-pass at eating

--- octoknife.rb
+++ octoknife.rb
@@ -1,11 +1,39 @@
 require 'curses'
+require 'pry'
 # TODO add sushi, love
+module Chance
+  def d100_beats?(check)
+    rand(100) > check
+  end
+end
+
+class UnmovingEntity
+  attr_reader :avatar, :x, :y
+  def initialize(x, y, avatar, board)
+    @x, @y, @avatar, @board = x, y, avatar, board
+    @present = true
+  end
+
+  def inverse_avatar; " " * 2 * avatar.length; end
+  def update; end
+
+  # TODO: update this when there's non-food items
+  def food?; true; end
+  def consume
+    raise "inedible!" unless food?
+    @board.remove_entity(self)
+  end
+end
+
 class Board
-  attr_reader :entities
+  include Chance
+
+  attr_reader :entities, :locations
 
   def initialize(win)
     @win = win
     @entities = []
+    @locations = Array.new(@win.maxx, [])
   end
 
   def add_entity(entity)
@@ -13,10 +41,18 @@ class Board
     borrow_cursor do |win|
       win.setpos(entity.y, entity.x)
       win.addstr(entity.avatar)
+      locations[entity.x][entity.y] ||= []
+      locations[entity.x][entity.y] << entity
     end
   end
 
+  def remove_entity(entity)
+    @entities.delete(entity)
+    locations[entity.x][entity.y].delete(entity)
+  end
+
   def render_step
+    add_food
     entities.each do |entity|
       update(entity)
     end
@@ -26,6 +62,9 @@ class Board
   # delegates...
   def maxx; @win.maxx; end
   def maxy; @win.maxy; end
+  def near(x,y)
+    (locations[x-1..x+1][y-1..y+1]&.flatten || []).compact
+  end
 
   private
   def update(entity)
@@ -38,6 +77,8 @@ class Board
 
       win.setpos(entity.y, entity.x)
       win.addstr(entity.avatar)
+      locations[entity.x][entity.y] ||= []
+      locations[entity.x][entity.y] << entity
     end
   end
 
@@ -46,26 +87,11 @@ class Board
     yield @win
     @win.setpos(old_y, old_x)
   end
-end
 
-module Chance
-  def d100_beats?(check)
-    rand(100) > check
-  end
-end
-
-class StaticEntity
-  def initialize(x, y, avatar, win)
-    @x, @y, @avatar, @win = x, y, avatar, win
-    @present = true
-  end
-
-  def draw
-
-  end
-
-  def consume
-    @present = false
+  def add_food
+    if d100_beats?(75)
+      add_entity(UnmovingEntity.new(rand(maxx - 5), rand(maxy - 2), ?🍣, self))
+    end
   end
 end
 
@@ -89,16 +115,22 @@ class Octopus
 
   def update
     change_direction if d100_beats?(direction_change_propensity)
-    case @direction
-    when 'UP'
-      @y == 1 ? @direction ='DOWN' : @y -=1
-    when 'DOWN'
-      @y == @board.maxy - 2 ? @direction = 'UP' : @y += 1
-    when 'LEFT'
-      @x == 1 ? @direction = 'RIGHT' : @x -=1
-    when 'RIGHT'
-      # The five here is again because the knife takes an additional character
-      @x >= @board.maxx - 5 ? @direction = 'LEFT' : @x += 1
+    nearby = @board.near(x,y)
+    if food = nearby&.find(&:food?)
+      @x, @y = food.x, food.y
+      consume(food)
+    else
+      case @direction
+      when 'UP'
+        @y == 1 ? @direction ='DOWN' : @y -=1
+      when 'DOWN'
+        @y == @board.maxy - 2 ? @direction = 'UP' : @y += 1
+      when 'LEFT'
+        @x == 1 ? @direction = 'RIGHT' : @x -=1
+      when 'RIGHT'
+        # The five here is again because the knife takes an additional character
+        @x >= @board.maxx - 5 ? @direction = 'LEFT' : @x += 1
+      end
     end
   end
 
@@ -106,9 +138,16 @@ class Octopus
     "<Octopus @x=#{@x} @y=#{@y}>"
   end
 
+  def food?; false; end
+
   private
   def change_direction
     @direction = DIRECTIONS.sample
+  end
+
+  def consume(food)
+    # TODO give a boon for well-fedness
+    food.consume
   end
 end
 
@@ -122,7 +161,7 @@ def render_screen
   Curses.init_screen
   win = Curses.stdscr
   board = Board.new(win)
-  octos = 10.times.map { |i| board.add_entity(Octopus.new(rand(win.maxx - 4), rand(win.maxy - 1), win)) }
+  octos = 10.times.map { |i| board.add_entity(Octopus.new(rand(win.maxx - 4), rand(win.maxy - 1), board)) }
 
   begin
     Curses.curs_set(0)
